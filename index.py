1e6af07e2fa1278e169733f577997da63082ab69
Marumasa <77374813+malken21@users.noreply.github.com>
2024-07-24 16:19:15 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,18 +1,30 @@
 from utils import read, save
 
+
+# ----- setting ----- start
+
 # sounds.json ファイルのパス
 TARGET_PATH = "./sounds.json"
-
+# 変換された sounds.json ファイルが生成されるパス
 RESULT_PATH = "./result.json"
+
+# 書き込まれるサウンド名
+AFTER_SOUND_NAME = "minuma/iku001"
+
+# ----- setting ----- end
+
 
 sounds = read(TARGET_PATH)
 
 for i in sounds:
-    for j in sounds[i]:
-        print(i)
-        print(j)
-        print(sounds[i][j])
-        print("\n")
+    for j in range(len(sounds[i]["sounds"])):
+        if type(sounds[i]["sounds"][j]) is str:
+            print(f"{sounds[i]["sounds"][j]} -> {AFTER_SOUND_NAME}\n")
+            sounds[i]["sounds"][j] = AFTER_SOUND_NAME
+        else:
+            print(f"{sounds[i]["sounds"][j]["name"]} -> {AFTER_SOUND_NAME}\n")
+            sounds[i]["sounds"][j]["name"] = AFTER_SOUND_NAME
+
 
 # 編集後の sounds.json ファイルを保存する
 save(RESULT_PATH, sounds)
